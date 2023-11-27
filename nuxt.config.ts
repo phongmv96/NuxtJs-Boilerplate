9e8098a755464b5a27caa340cb9eb10c08089574
phong.mv <phongmv@ycomm.vn>
2023-11-27 16:53:23 +0700

    Test pre-commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    srcDir: 'src/',
-    modules: [
-        '@nuxtjs/eslint-module'
-    ]
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  modules: ['@nuxtjs/eslint-module']
 })
---
+++ src/components/demo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="demo">PhongMai</div>
+</template>
+
+<script setup lang="ts">
+// const a = 'phong'
+</script>
+<style scoped></style>
