5f6c2a58b38e0bf099095565c9d077e7e798cac4
phong.mv <phongmv@ycomm.vn>
2023-11-30 10:19:15 +0700

    Test absolute import

--- src/components/chapter/chapter-filter/ChapterFilter.vue
+++ src/components/chapter/chapter-filter/ChapterFilter.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import noDataImage from '~/assets/icons/noData.png'
+</script>
 
 <template>
-  <div class="chapter-filter">Chapter filter {{ isEdit.value }}</div>
+  <div class="chapter-filter">
+    Chapter filter
+    <img :src="noDataImage" alt="nodata" />
+  </div>
 </template>
 
 <style scoped src="./_style.scss"></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,6 @@
   <ComponentA />
   <LqButton />
   <LqSelect />
+  <ChapterFilter />
 </template>
 <script setup lang="ts"></script>
