85c5ff0f6b109af562e26aac274746aa0cef6bec
phong.mv <phongmv@ycomm.vn>
2023-12-11 15:55:07 +0700

    Update project structure

--- src/components/component-a/ComponentA.vue
+++ src/components/component-a/ComponentA.vue
@@ -2,9 +2,6 @@
   <div @click.prevent="useFoo" class="component-a cursor-pointer">
     Component A - auto import composable useFoo
   </div>
-  <div @click.prevent="useBar" class="component-a cursor-pointer">
-    Component A - auto import composable useBar
-  </div>
 </template>
 
 <script lang="ts" setup></script>
--- src/composables/useBar.ts
+++
@@ -1,3 +0,0 @@
-export const useBar = () => {
-  console.log('useBar')
-}
